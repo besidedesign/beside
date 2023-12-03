37a6ee0f6b040ba9a042febd98b7038f71ff22ca
besidedesign <besidesocials@gmail.com>
2023-12-03 17:32:54 +0100

    Middleware Update

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,11 @@ import { i18n } from "@/i18n.config";
 import { match as matchLocale } from "@formatjs/intl-localematcher";
 import Negotiator from "negotiator";
 
+// Normalisierungsfunktion für Locales
+function normalizeLocale(locale: string): string {
+  return locale.split("-")[0];
+}
+
 function getLocale(request: NextRequest): string | undefined {
   const negotiatorHeaders: Record<string, string> = {};
   request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
@@ -14,15 +19,16 @@ function getLocale(request: NextRequest): string | undefined {
   const locales: string[] = i18n.locales;
   const languages = new Negotiator({ headers: negotiatorHeaders }).languages();
 
-  const locale = matchLocale(languages, locales, i18n.defaultLocale);
+  const normalizedLanguages = languages.map(normalizeLocale);
+  const locale = matchLocale(normalizedLanguages, locales, i18n.defaultLocale);
+
   return locale;
 }
 
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
   const pathnameIsMissingLocale = i18n.locales.every(
-    (locale) =>
-      !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`,
+    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
   );
 
   // Redirect if there is no locale
@@ -31,8 +37,8 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(
       new URL(
         `/${locale}${pathname.startsWith("/") ? "" : "/"}${pathname}`,
-        request.url,
-      ),
+        request.url
+      )
     );
   }
 }
